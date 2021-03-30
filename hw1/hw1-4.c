f3a170ed43ef686412a39c0276b7107fa126f342
anila <asdfg335522@gmail.com>
2021-03-30 23:19:08 +0800

    2021.03.30

--- hw1/hw1-4.c
+++ hw1/hw1-4.c
@@ -6,22 +6,6 @@ typedef struct _count {
     int64_t key;
 }count;
 
-void cnt(count list[], int64_t begin, int64_t end) {
-    int64_t mid = (begin + end)/2;
-    if(begin == end) {
-        return ;
-    }
-    cnt(list,begin,mid);
-    cnt(list,mid+1,end);
-    for(int64_t i = mid+1; i <= end ; i++) {
-        for(int64_t j = begin ; j < mid+1 ; j++) {
-            if(list[j].key < list[i].key) {
-                list[i].sum += list[j].key;
-            }
-        }
-    }
-}
-
 void merge(count list[],count sorted[],int64_t begin,int64_t end) {
     int64_t mid = (begin + end)/2;
     if(begin == end) {
@@ -29,6 +13,18 @@ void merge(count list[],count sorted[],int64_t begin,int64_t end) {
     }
     merge(list,sorted,begin,mid);
     merge(list,sorted,mid+1,end);
+    int64_t j = begin;
+    int64_t tmp = 0;
+    for(int64_t i = mid+1 ; i <= end ; i++) {
+        if(i > mid+1) {
+            list[i].sum += tmp;
+        }
+        while(j < mid+1 && list[j].key < list[i].key) {
+            tmp += list[j].key;
+            list[i].sum += list[j].key;
+            j += 1;
+        }
+    }
     int64_t lindex = begin;
     int64_t rindex = mid+1;
     int64_t sindex = begin;
@@ -73,14 +69,6 @@ void merge(count list[],count sorted[],int64_t begin,int64_t end) {
         list[i].key = sorted[i].key;
         list[i].sum = sorted[i].sum;
     }
-    // for(int64_t i = begin ; i < sindex ; i++) {
-    //     printf("%ld ",sorted[i].key);
-    // }
-    // printf("\n");
-    // for(int64_t i = begin ; i < sindex ; i++) {
-    //     printf("%ld ",sorted[i].sum);
-    // }
-    // printf("\n");
 }
 
 int main() {
@@ -93,23 +81,14 @@ int main() {
         arr[i].sum = 0;
         sorted[i].sum = 0;
     }
-    cnt(arr,0,2*num-1);
-    merge(arr,sorted,0,2*num-1); 
-    // for(int64_t i = 0 ; i < 2*num ; i++) {
-    //     printf("%ld ",sorted[i].key);
-    // }
-    // printf("\n");
-    // for(int64_t i = 0 ; i < 2*num ; i++) {
-    //     printf("%ld ",sorted[i].sum);
-    // }
-    // printf("\n");
+    merge(arr,sorted,0,(2*num-1));
     int64_t ans = 0;
     for(int64_t i = 0 ; i < 2*num ; i += 2) {
-        if((sorted[i+1].sum-sorted[i].sum) < 0) {
-            ans += (sorted[i].sum-sorted[i+1].sum);
+        if((arr[i+1].sum-arr[i].sum) < 0) {
+            ans += (arr[i].sum-arr[i+1].sum);
         }
         else{
-            ans += (sorted[i+1].sum-sorted[i].sum);
+            ans += (arr[i+1].sum-arr[i].sum);
         }
     }
     printf("%ld\n",ans);
