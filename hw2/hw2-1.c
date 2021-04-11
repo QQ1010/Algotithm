b7bbf8cac1d5cd4e68dc465043fef2738811c8b5
QQ <asdfg335522@gmail.com>
2021-04-11 20:01:17 +0800

    2020.04.10

---
+++ hw2/hw2-1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+    int32_t num = 0;
+    int32_t maxbag = 0;
+    int32_t weight[101] = {0};
+    int32_t value[101] = {0};
+    int32_t n_tmp = num+1;
+    int32_t b_tmp = maxbag+1;
+    int64_t table[n_tmp][b_tmp];
+    scanf("%d %d",&num,&maxbag);
+    for(int32_t i = 1 ; i <= num ; i++) {
+        scanf("%d %d",&weight[i],&value[i]);
+    }
+    for(int32_t i = 1 ; i <= num ; i++) {
+        for(int32_t j = 1 ; j <= maxbag ; j++) {
+            if(i == 0) 
+                table[i][j] = 0;
+            else if( j == 0)
+                table[i][j] = 0;
+        }
+    }
+    for(int32_t i = 0 ; i < num+1 ; i++) {
+        for(int32_t  j = 0 ; j < maxbag+1 ; j++) {
+            if(j - weight[i] < 0) {
+                table[i][j] = table[i-1][j];
+            }
+            else if(j - weight[i] >= 0) {
+                if(table[i-1][j] < (table[i-1][j-weight[i]] + value[i])) {
+                    table[i][j] = table[i-1][j-weight[i]] + value[i];
+                }
+                else {
+                    table[i][j] = table[i-1][j];
+                }
+            }
+        }
+    }
+    for(int32_t i = 0 ; i < num+1 ; i++) {
+        for(int32_t j = 0 ; j < maxbag+1 ; j++) {
+            printf("%ld ",table[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
