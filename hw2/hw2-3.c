4b68c83d9b824b4d3513a874e32196c30a0384c7
QQ <asdfg335522@gmail.com>
2021-04-20 09:28:16 +0800

    2021.04.20 AC

--- hw2/hw2-3.c
+++ hw2/hw2-3.c
@@ -7,6 +7,48 @@ typedef struct _list {
     int32_t y;
 }list;
 
+int main() {
+    int32_t num = 0;
+    scanf("%d",&num);
+    list point[num];
+    int32_t len[num];
+    int32_t tmp[num];
+    for(int32_t i = 0 ; i < num ; i++) {
+        scanf("%d %d",&point[i].x,&point[i].y);
+    }
+    for(int32_t i = 0 ; i < num ; i++) {              //bubble_sort in x
+        for(int32_t j = i; j < num ; j++) {
+            list tmp;
+            if(point[i].x > point[j].x) {
+            tmp = point[i];
+            point[i] = point[j];
+            point[j] = tmp;
+            }
+        }
+    }
+    for(int32_t i = 0 ; i < num ; i++) {    // len initial = 1
+        len[i] = 1;
+    }
+    for(int32_t i = 0 ; i < num ; i++) {    // do LIS in y
+        tmp[i] = point[i].y;
+        for(int32_t j = 0 ; j < i ; j++) {
+            if(tmp[j] <= tmp[i]) {
+                int32_t q = len[j]+1;
+                if(q > len[i])
+                    len[i] = q;
+            }
+        }
+    }
+    int32_t ans = 0;
+    for(int32_t i = 0 ; i < num ; i++) {
+        if(ans < len[i])
+            ans = len[i];
+    }
+    printf("%d\n",ans);
+    return 0;
+}
+
+/*q sort not work need to strudy */
 // int32_t cmp(list * a, list * b) {
 //    if(a->x < b->x)
 //         return -1;
@@ -17,33 +59,7 @@ typedef struct _list {
         
 // }
 
-int main() {
-    int32_t num = 0;
-    scanf("%d",&num);
-    list point[num];
-    int32_t len[num];
-    int32_t tmp[num];
-    for(int32_t i = 0 ; i < num ; i++) {
-        scanf("%d %d",&point[i].x,&point[i].y);
-    }
-    for(int32_t i = 0 ; i < num ; i++) {              //bubble_sort
-        for(int32_t j = i; j < num ; j++) {
-            list tmp;
-            if(point[i].x > point[j].x) {
-            tmp = point[i];
-            point[i] = point[j];
-            point[j] = tmp;
-            }
-        }
-    }
-    // qsort(point,num,sizeof(list),cmp);             //qsort -> not yet done
+// qsort(point,num,sizeof(list),cmp);             //qsort -> not yet done
     // for(int32_t i = 0 ; i < num ; i++) {
     //     printf("%d %d\n",point[i].x,point[i].y);
     // }
-    for(int32_t i = 0 ; i < num ; i++) {    // len initial = 1
-        len[i] = 1;
-    }
-    for(int32_t i = 0 ; i < num ; i++) {    // do LIS
-        
-    }
-}
